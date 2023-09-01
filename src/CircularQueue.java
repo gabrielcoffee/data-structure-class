133dc380fae6c537daf2243b2e92526c610ad10f
Gabriel Fernandes <60583301+gabrielcoffee@users.noreply.github.com>
2023-09-01 08:36:27 -0400

    fix: circular queue

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -1,86 +1,69 @@
-/*
-
-!!!NOTA IMPORTANTE!!
-ESSA IMPLEMENTAÇÃO DA MINHA CIRCULAR QUEUE TEM UM DETALHE DIFERENTE:
-AO ADICIONAR ALGUM NÚMERO COM A LISTA CHEIA ELA ADICIONA NA POSIÇÃO DO
-PRIMEIRO NÚMERO A TER SIDO ADICIONADO OU SEJA O MAIS ANTIGO
-
-TEM VARIAS IMPLEMENTAÇÕES E VIDEOS NA INTERNET MOSTRANDO COMO IMPLEMENTAR
-DA FORMA NORMAL (QUE É DE FATO O QUE A PROFESSORA PEDIU)
-
-MAS SE TIVEREM ALGUMA DÚVIDA SEMPRE PODEM ME CHAMAR NO DISCORD https://discord.gg/n4JwA7nF
-E SE PRECISAR DE UMA CALL POSSO ATENDER DAS 8H AS 9H NA TERÇA OU AS VEZES QUALQUER DIA A NOITE
-
-GABRIEL FERNANDES
-
- */
+// NESTA IMPLEMENTAÇÃO DEFINI VALORES DELETADOS/INDEFINIDOS COMO ZERO
 
 public class CircularQueue {
 
     private int top;
     private int base;
     private int[] data;
-    private int size;
 
     public CircularQueue(int size) {
         this.top =  -1;
         this.base = -1;
-        this.size = size;
         this.data = new int[size];
     }
 
-    public void add(int data) {
-        if (isEmpty()) {
-            base = 0;
-        }
-
+    public void add(int value) {
         if (isFull()) {
-            base = (base + 1) % size;
+            System.out.println("list is full");
+            return;
         }
-
-        top = (top + 1) % size;
-        this.data[top] = data;
+        if (isEmpty()) {
+            base++;
+        }
+        top = (top + 1) % data.length;
+        data[top] = value;
     }
 
     public int remove() {
-        if (!isEmpty()) {
-            int tmp = data[base];
-            this.data[base] = -1;
-            base = (base + 1) % size;
-            return tmp;
+        if (isEmpty()) {
+            System.out.println("list is empty");
+            return -1;
         }
+        int tmp = data[base];
+        data[base] = 0;
 
-        return -1;
+        if (base == top) {
+            base = -1;
+            top = -1;
+        }
+        else {
+            base = (base + 1) % data.length;
+        }
+        return tmp;
     }
 
     public void clear() {
         if (!isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                data[i] = -1;
+            for (int i = 0; i < data.length; i++) {
+                data[i] = 0;
             }
         }
         top = -1;
         base = -1;
     }
 
-    public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.println(i + ": " + this.data[i]);
-        }
-        System.out.println();
-    }
-
     private boolean isFull() {
-        if ((top + 1) % size == base) {
-            return true;
-        }
-        return false;
+        return (top + 1) % data.length == base;
     }
 
     private boolean isEmpty() {
-        if (base == -1 && top == -1) {
-            return true;
+        return top == -1;
+    }
+
+    public void print() {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(i + ": " + this.data[i]);
         }
-        return false;
+        System.out.println();
     }
 }
