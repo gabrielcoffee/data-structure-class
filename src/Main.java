71bdd1715a7b459f04b3dce7e0fa1eb6128badc7
Gabriel Fernandes <60583301+gabrielcoffee@users.noreply.github.com>
2023-11-13 09:13:24 -0300

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         int[] nums = {49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28};
 
-        InsertionSort.sort(nums);
+        QuickSort.sort(nums);
 
     }
 }
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,6 +1,41 @@
+import java.util.Arrays;
+
 public class QuickSort {
 
+    static int step = 1;
+
+    private static int[] sortRecursive(int start, int end, int[] nums) {
+        int marker = 0;
+        int pivot = nums[nums.length-1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= pivot) {
+                marker++;
+            }
+
+            if (marker < i) {
+                int tmp = nums[i];
+                nums[i] = nums[marker];
+                nums[marker] = tmp;
+
+                print(step, nums);
+                step++;
+            }
+        }
+
+        sortRecursive(0, marker, nums);
+        sortRecursive(marker+1, nums.length, nums);
+
+        return nums;
+    }
+
     public static int[] sort(int[] nums) {
-        return null;
+        int[] result = sortRecursive(0, nums.length, nums);
+        step = 1;
+        return result;
+    }
+
+    private static void print(int step, int[] nums) {
+        System.out.println(step + "  ->  " + Arrays.toString(nums));
     }
 }
